c51c764326b7a9b56428c237f2a6e67e0fd21059
Aldert Vaandering <paperfeed@gmail.com>
2021-04-25 15:39:58 +0200

    Made Enum utility

--- src/Server/utils.ts
+++ src/Server/utils.ts
@@ -1,4 +1,7 @@
+import { Message } from 'discord.js'
 import fetch from 'node-fetch'
+
+import { Emoji } from './Data/Emoji'
 
 export async function fetchAsync<T>(url: string): Promise<T> {
   const result = await fetch(url)
@@ -12,3 +15,33 @@ export const UrlBuildParams = (obj: Record<string, string>) =>
 
 export const ifArrayGetFirstItem = <T = unknown>(a: T[] | undefined) =>
   a && a.length ? a[0] : undefined
+
+export class EmojiHelper {
+  static addReactions = async (emojis: (Emoji | string)[], msg: Message) => {
+    for (const emoji of emojis) {
+      await msg.react(emoji)
+    }
+  }
+}
+
+type ValueOf<T> = T[keyof T]
+
+export class Enum {
+  public static keys<T extends Record<string, string | number>>(obj: T) {
+    return Object.keys(obj).filter(i =>
+      isNaN(Number(i)),
+    ) as (keyof typeof obj)[]
+  }
+
+  public static values<T extends Record<string, string | number>>(obj: T) {
+    return Object.keys(obj)
+      .filter(i => isNaN(Number(i)))
+      .map(key => obj[key]) as ValueOf<T>[]
+  }
+
+  public static entries<T extends Record<string, string | number>>(obj: T) {
+    return Object.keys(obj)
+      .filter(i => isNaN(Number(i)))
+      .map(key => [key, obj[key]]) as [keyof typeof obj, ValueOf<T>][]
+  }
+}
