2b159759cc41c421bf7d508911115ce78794f511
Aldert Vaandering <paperfeed@gmail.com>
2021-05-01 14:31:00 +0200

    Added bora insults (jooookes, relax)

---
+++ src/Server/Data/BoraInsults.ts
@@ -0,0 +1,15 @@
+export const BoraInsults = [
+  'Bora is kkr gay',
+  'houd je bek, Bora',
+  'bora plz',
+  'Bora, houd je bek',
+  'Bora stop met praten',
+  'Bora, tyf op',
+  'Doei Bora',
+  'fk off, bora',
+  'ga lekker hamsteren ofzo, Borat',
+  'no u',
+  'niemand is geinterreerd in je Oneplus camera',
+  'Calculating chance Bora is whining... Result: 99.9999998%',
+  '`uuuuuughhhhhhhhhh` -- Bora',
+]
